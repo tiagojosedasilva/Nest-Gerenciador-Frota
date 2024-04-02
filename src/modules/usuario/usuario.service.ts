b03bba1acee1cf89b37f9966c2e10a02aa2bd9f8
tiagojosedasilva <tiago.estudo.desenvolvimento@gmail.com>
2024-04-02 14:28:30 -0300

    tratando erros

--- src/modules/usuario/usuario.service.ts
+++ src/modules/usuario/usuario.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject } from "@nestjs/common";
+import { Injectable, Inject, ForbiddenException } from "@nestjs/common";
 import { Repository } from "typeorm";
 import { CreateUsuarioDto } from "./dto/create-usuario.dto";
 import { UpdateUsuarioDto } from "./dto/update-usuario.dto";
@@ -13,27 +13,57 @@ export class UsuarioService {
   ){}
 
   async create(createUsuarioDto: CreateUsuarioDto) {
-    return await this.usuarioRepository.save(createUsuarioDto)
+    try {
+      return await this.usuarioRepository.save(createUsuarioDto)
+    } catch (error) {
+      console.error(error)
+      throw new ForbiddenException(error)
+    }
   }
 
   async findAll() {
-    return await this.usuarioRepository.find()
+    try {
+      return await this.usuarioRepository.find()
+    } catch (error) {
+      console.error(error)
+      throw new ForbiddenException(error)
+    }
   }
 
   async findOne(id: number) {
-    return await this.usuarioRepository.findOneBy({id});
+    try {
+      return await this.usuarioRepository.findOneBy({id});
+    } catch (error) {
+      console.error(error)
+      throw new ForbiddenException(error)
+    }
   }
 
   async findOneForEmail(email: string){
-    return await this.usuarioRepository.findOne({where: {email: email}})
+    try {
+      return await this.usuarioRepository.findOne({where: {email: email}})
+    } catch (error) {
+      console.error(error)
+      throw new ForbiddenException(error)
+    }
   }
 
   async update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
-    return await this.usuarioRepository.update(id, updateUsuarioDto)
+    try {
+      return await this.usuarioRepository.update(id, updateUsuarioDto)
+    } catch (error) {
+      console.error(error)
+      throw new ForbiddenException(error)
+    }
   }
 
   async remove(id: number) {
-    return await this.usuarioRepository.delete(id)
+    try {
+      return await this.usuarioRepository.delete(id)
+    } catch (error) {
+      console.error(error)
+      throw new ForbiddenException(error)
+    }
   }
 
 }
